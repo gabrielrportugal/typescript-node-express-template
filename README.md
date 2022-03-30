66e0411f65800e17becec7ddf81484e209e10a5d
gabrielrportugal <gabrielrportugal@outlook.com>
2022-03-30 18:30:58 -0300

    update template

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# typescript-node-express
+# rentalx
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+
+app.get('/ping', (request: Request, response: Response) =>
+  response.send('pingou')
+);
 
 app.listen(3333, () => console.log('🚀 Server started on port 3333'));
